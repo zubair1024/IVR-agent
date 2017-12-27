18ced6193daf535c17c47b803e526350da3ea7d9
Zubair Ahmed <zubair1024@gmail.com>
2017-12-27 15:35:37 +0400

    first commit

---
+++ survey_data.js
@@ -0,0 +1,15 @@
+// Hard coded survey questions
+module.exports = [
+    {
+        text: 'Please tell us your number',
+        type: 'number'
+    },
+    {
+        text: 'Please state your door number or house name',
+        type: 'text'
+    },
+    {
+        text: 'Please state the make and model of the boiler',
+        type: 'text'
+    }
+];
